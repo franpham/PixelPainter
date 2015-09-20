88efae86bc60f4cb94a331382c969920b2e1808e
franpham <franpham@gmail.com>
2015-09-19 23:25:22 -1000

    added untested code to select, move, & copy pixels

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -3,17 +3,20 @@ function PixelPainter(width, height) {
   var defColors = ['white', 'silver', 'gray', 'black', 'red', 'maroon', 'yellow', 'olive', 'lime', 'green', 'aqua', 'teal', 'blue', 'navy', 'fuchsia', 'purple'];
   var grid = document.createElement('div');
   var topbar = document.createElement('div');
-
   var buttons = new Array(width);
+
   var swatch = new Array(16);
   var erase = document.createElement('button');
   var clear = document.createElement('button');
-  var moved = document.createElement('button');
+  var select = document.createElement('button');
+  var move = document.createElement('button');
+  var copy = document.createElement('button');
   var lastButton = thisButton = null;
-  var isPainting = isErasing = isMoving = false;
+  var isPainting = isErasing = false;
+  var isSelect = isMove = isCopy = false;
   var moveFirst = moveLast = moveNew = -1;
-  var selected = 'white';
-  var moving = [];
+  var selected = 'white';   // the selected color;
+  var selection = [];       // the selected buttons;
 
   // make the swatch buttons and register event listeners;
   for (var i = 0; i < swatch.length; i++) {
@@ -24,20 +27,6 @@ function PixelPainter(width, height) {
       selected = this.style.background;
     });
   }
-
-  // set properties and add listeners for controls buttons
-  erase.className = 'swatchButton';
-  clear.className = 'swatchButton';
-  moved.className = 'swatchButton';
-  erase.appendChild(document.createTextNode('Erase'));
-  clear.appendChild(document.createTextNode('Clear'));
-  moved.appendChild(document.createTextNode('Select & Move'));
-  moved.addEventListener('click', function() {
-    isMoving = true;
-  });
-  erase.addEventListener('click', function() {
-    selected = 'white';
-  });
   clear.addEventListener('click', function() {
     for (var i = 0; i < width; i++) {
       for (var j = 0; j < height; j++) {
@@ -45,9 +34,32 @@ function PixelPainter(width, height) {
       }
     }
   });
-  topbar.appendChild(clear);
+
+  // set properties and add listeners for controls buttons
+  erase.addEventListener('click', function() {
+    selected = 'white';
+  });
+  select.addEventListener('click', function() {
+    isSelect = true;
+  });
+  move.addEventListener('click', function() {
+    isMove = true;
+  });
+  copy.addEventListener('click', function() {
+    isCopy = true;
+  });
+  erase.className = clear.className = 'swatchButton';
+  select.className = move.className = copy.className = 'swatchButton';
+  erase.appendChild(document.createTextNode('Erase'));
+  clear.appendChild(document.createTextNode('Clear'));
+  select.appendChild(document.createTextNode('Select'));
+  move.appendChild(document.createTextNode('Move'));
+  copy.appendChild(document.createTextNode('Copy'));
   topbar.appendChild(erase);
-  topbar.appendChild(moved);
+  topbar.appendChild(clear);
+  topbar.appendChild(select);
+  topbar.appendChild(move);
+  topbar.appendChild(copy);
 
   // make the grid buttons and register event listeners;
   for (var i = 0; i < width; i++) {
@@ -57,17 +69,17 @@ function PixelPainter(width, height) {
       buttons[i][j].className = 'button';
       // buttons[i][j].name = 'button' + i + '/' + j;   // FOR DEBUGGING;
 
-      buttons[i][j].addEventListener('click', function() {
+      buttons[i][j].addEventListener('click', function() {    // handler to start AND stop painting and erasing;
         isPainting = !isPainting;
         if (isPainting)
           this.style.background = selected;
       });
-      buttons[i][j].dataset.lastColor = 'white';
-      buttons[i][j].addEventListener('mouseover', function() {
+      buttons[i][j].dataset.lastColor = 'white';            // custom data to record previous color;
+      buttons[i][j].addEventListener('mouseover', function() {    // handler to paint and erase on moveover;
         // IMPLEMENTATION NOTE: erasing does not start until cursor moves back to the previous button!
         if (isPainting) {
           if (this.style.background === selected && (lastButton === this || isErasing)) {
-            lastButton.style.background = this.style.background = 'white';
+            lastButton.style.background = this.style.background = this.dataset.lastColor; // 'white';
             isErasing = true;
           }
           else {
@@ -78,61 +90,59 @@ function PixelPainter(width, height) {
           thisButton = this;
         }
       });
-      buttons[i][j].dataset.listIndex = (i * j) + i;
-      buttons[i][j].addEventListener('click', function() {
-        if (!isMoving)    // DO NOT combine conditions: must check moveFirst, moveLast, moveNew in sequence!
-          return;
-        else if (moveFirst === -1) {  // start move selection;
-          moveFirst = this.dataset.listIndex;
-        }
-        else if (moveLast === -1) {    // finish move selection;
-          moveLast = this.dataset.listIndex;
-          moving = buttons.slice(moveFirst, moveLast - moveFirst + 1);
-        }
-        else if (moveNew === -1) {
-          moveNew = this.dataset.listIndex;
-        }
-        else if (moveNew > -1) {
-          for (var i = 0; i < moving.length; i++) {
-            buttons[moveFirst + i + moveNew].style.background = moving[i].style.background;
-            moving[i].style.background = 'white';
-            moveFirst = moveLast = moveNew = -1;    // RESET all move variables after moving!
-            isMoving = false;
+      buttons[i][j].dataset.listIndex = (i * j) + i;    // custom data to save button's overall index position;
+      buttons[i][j].addEventListener('click', function() {    // handler to select, selection, and "move" buttons;
+        if (isSelect) {
+          if (moveFirst === -1) {   // start selection;
+            moveFirst = this.dataset.listIndex;
           }
+          else if (moveLast === -1) {   // finish selection;
+            moveLast = this.dataset.listIndex;
+            selection = buttons.slice(moveFirst, moveLast - moveFirst + 1);
+            isSelect = false;     // DO NOT reset moveFirst && moveLast!
+          }
+        }
+        else if ((isMove || isCopy) && moveNew === -1) {
+          moveNew = this.dataset.listIndex;
+          for (var i = 0; i < selection.length; i++) {
+            buttons[moveFirst + i + moveNew].style.background = selection[i].style.background;
+            if (isMove)
+              selection[i].style.background = 'white';
+          }
+          moveFirst = moveLast = moveNew = -1;    // RESET all move variables!
+          isMove = isCopy = false;
         }
       });
     }
   }
 
   // add buttons to a grid of div rows;
+  grid.className = 'spacing';
   for (var i = 0; i < width; i++) {
     var temp = document.createElement('div');
-
     for (var j = 0; j < height; j++) {
       temp.appendChild(buttons[i][j]);
     }
     grid.appendChild(temp);
   }
-  grid.className = 'spacing';
-
-  // add the swatch buttons to a div grid;
+  // add the swatch buttons to 2 div rows;
   var row1 = document.createElement('div');
   var row2 = document.createElement('div');
-  row1.className = 'spacing';
-  row2.className = 'spacing';
   for (var i = 0; i < swatch.length; i++) {
     if (i < swatch.length / 2)
       row1.appendChild(swatch[i]);
     else
       row2.appendChild(swatch[i]);
   }
-  var hr = document.createElement('hr');
-  hr.className = 'spacing';
+  var info = document.createElement('div');   // DO NOT chain createTextNode, else info will be set to a TextNode;
+  info.appendChild(document.createTextNode('Click to start drawing or selecting, then click again to stop.'));
+  info.className = 'spacing info';
+  topbar.className = 'spacing';
   topbar.appendChild(row1);
   topbar.appendChild(row2);
-  topbar.appendChild(hr);
+  topbar.appendChild(info);
   document.getElementById('pixelPainter').appendChild(topbar);
   document.getElementById('pixelPainter').appendChild(grid);
 }
 
-var painter = new PixelPainter(8, 8);
+var painter = new PixelPainter(10, 10);
